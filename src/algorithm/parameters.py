3387d7079278ea892768cbcf52c3412ecb14b4fc
frarandone <frarandone@gmail.com>
2025-01-25 10:44:46 +0100

    Burglary results

--- src/algorithm/parameters.py
+++ src/algorithm/parameters.py
@@ -18,12 +18,12 @@ params = {
     'SCHC_COUNT_METHOD': "count_all", 
 
     # Set optional experiment name
-    'EXPERIMENT_NAME': 'easytugwar_sketch1_30', # name of the folder
-    'PROGRAM_NAME': 'easytugwar',
+    'EXPERIMENT_NAME': 'burglary_sketch2', # name of the folder
+    'PROGRAM_NAME': 'burglary',
     'DEPENDENCIES_BENEFIT': False,
     # Set default number of runs to be done.
     # ONLY USED WITH EXPERIMENT MANAGER.
-    'RUNS': 12,
+    'RUNS': 15,
 
     # Class of problem
     #'FITNESS_FUNCTION': "supervised_learning.regression",
@@ -36,7 +36,7 @@ params = {
 
     # Set grammar file
     #'GRAMMAR_FILE': "soga_holes.pybnf",
-    'GRAMMAR_FILE': "soga_csi_sketch3.pybnf",
+    'GRAMMAR_FILE': "soga_burglary_sketch2.pybnf",
 
     # Set the number of depths permutations are calculated for
     # (starting from the minimum path of the grammar).
